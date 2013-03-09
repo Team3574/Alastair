ee5b6386285ad1dcaccec252fc355861dd995897
Team3574 <d.b.nowlin@comcast.net>
2013-03-09 11:08:07 -0800

    pre 87
    angle changed

--- src/edu/wpi/first/wpilibj/templates/Constants.java
+++ src/edu/wpi/first/wpilibj/templates/Constants.java
@@ -25,7 +25,7 @@ public class Constants {
     
     public static final int TILT_CROSS_COURT = (-519);
     public static final int TILT_MID_COURT = (-821);
-    public static final int TILT_PYRIMID_BACK = (-901);
+    public static final int TILT_PYRIMID_BACK = (-936);
     public static final int TILT_PYRIMID_MIDDLE = (-1000);
     public static final int TILT_PYRIMID_FRONT = (-1238);
     public static final int TILT_PYRIMID_TOP = (-1280);
