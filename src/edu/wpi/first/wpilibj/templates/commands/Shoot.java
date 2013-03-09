c7c43f27c14ef434b30dbdaa460c5919b8cdde3d
Team3574 <d.b.nowlin@comcast.net>
2013-03-09 09:33:10 -0800

    Portland Pre 7th match
    -Put a Variable of shoot on the drivers staition for easy modification.

--- src/edu/wpi/first/wpilibj/templates/commands/Shoot.java
+++ src/edu/wpi/first/wpilibj/templates/commands/Shoot.java
@@ -4,6 +4,7 @@
  */
 package edu.wpi.first.wpilibj.templates.commands;
 
+import edu.wpi.first.wpilibj.DriverStation;
 import team.util.LogDebugger;
 
 /**
@@ -25,11 +26,11 @@ public class Shoot extends CommandBase {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-        if (this.timeSinceInitialized() < 0.25) {
+        if (this.timeSinceInitialized() < DriverStation.getInstance().getAnalogIn(1)) {
             theShootingRam.kickerOut();
         }
-        else {
-            theShootingRam.kickerIn();
+	else{
+	    theShootingRam.kickerIn();
         }
     }
 
